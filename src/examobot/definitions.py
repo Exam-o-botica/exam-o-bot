db9ef1699df0ca91e7695106142066180dae4b96
Fyodor Vanchugov <fyodor.vanchugov@waveaccess.ru>
2024-01-26 13:22:06 +0300

    Optimize language definitions, add LocalizeInterface for future

--- src/examobot/definitions.py
+++ src/examobot/definitions.py
@@ -16,21 +16,22 @@ DEFAULT_DB_FILE = os.path.join(DB_DIR, "examobot_db.db")
 TOKEN = os.getenv("EXAM_O_BOT_TOKEN")
 DEFAULT_USER_AGENT = "Mozilla/5.0 (Windows NT 6.1; rv:84.0) Gecko/20100101 Firefox/84.0"
 
+
 # For localization
-LANGUAGE = "EN"
-DEFAULT_LANGUAGE = "EN"
-LOCALIZATIONS_DIR = os.path.join(ROOT_DIR, "../../localizations")
-DEFAULT_LOCALIZATION_FILE_NAME = "en.json"
-
-
 class SupportedLanguages(enum.Enum):
     EN = "en"
     RU = "ru"
 
     @staticmethod
-    def get_language(language: str) -> Optional['SupportedLanguages']:
+    def to_enum(language: str) -> Optional['SupportedLanguages']:
         for lang in SupportedLanguages:
             if lang.value.lower() == language.lower():
                 return lang
 
         return None
+
+
+LANGUAGE = SupportedLanguages.EN
+DEFAULT_LANGUAGE = SupportedLanguages.EN
+LOCALIZATIONS_DIR = os.path.join(ROOT_DIR, "../../localizations")
+DEFAULT_LOCALIZATION_FILE_NAME = "en.json"
