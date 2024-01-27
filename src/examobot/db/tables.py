bfc2c66406c252cdc55d924f08dae2cc0b7c6ce1
Fyodor Vanchugov <fyodor.vanchugov@waveaccess.ru>
2024-01-27 19:22:54 +0300

    Add new registy tables, add uuid fields

--- src/examobot/db/tables.py
+++ src/examobot/db/tables.py
@@ -88,20 +88,26 @@ class User(Base):
 class Classroom(Base):
     __tablename__ = 'classrooms'
 
-    id: Mapped[str] = mapped_column(primary_key=True, autoincrement=False)
-    title: Mapped[str] = mapped_column(primary_key=False)
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    uuid: Mapped[str] = mapped_column(index=True, autoincrement=False)
 
+    title: Mapped[str] = mapped_column(primary_key=False)
     author_id: Mapped[int] = mapped_column(ForeignKey("users.id"), nullable=False)
     author: Mapped[User] = relationship(back_populates="created_classrooms")  # Child
 
     participants: Mapped[List[User]] = relationship(
         secondary=classroom_user_table, uselist=True, back_populates='classrooms')
 
+    classroom_test_connections: Mapped[List["ClassroomTestConnection"]] = (
+        relationship(back_populates="classroom", cascade="all,delete", uselist=True))  # Parent
+
 
 class Test(Base):
     __tablename__ = 'tests'
 
-    id: Mapped[str] = mapped_column(primary_key=True, autoincrement=False)
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    uuid: Mapped[str] = mapped_column(index=True, autoincrement=False)
+
     title: Mapped[str] = mapped_column(nullable=False, unique=True, index=True)
     time: Mapped[int] = mapped_column(nullable=False, default=-1)
     deadline: Mapped[int] = mapped_column(nullable=False, default=-1)
@@ -114,12 +120,16 @@ class Test(Base):
     participants: Mapped[List[User]] = relationship(
         secondary=test_user_table, uselist=True, back_populates='tests')
     tasks: Mapped[List["Task"]] = relationship(back_populates="test", cascade="all,delete")  # Parent
+    classroom_test_connections: Mapped[List["ClassroomTestConnection"]] = (
+        relationship(back_populates="test", cascade="all,delete", uselist=True))  # Parent
 
 
 class Task(Base):
     __tablename__ = 'tasks'
 
-    id: Mapped[str] = mapped_column(primary_key=True, autoincrement=False)
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    uuid: Mapped[str] = mapped_column(index=True, autoincrement=False)
+
     order_id: Mapped[int] = mapped_column(nullable=False, default=1)
     title: Mapped[Optional[str]] = mapped_column(nullable=True, default=None)
     text: Mapped[str] = mapped_column(nullable=False)
@@ -147,7 +157,9 @@ class Task(Base):
 class Answer(Base):
     __tablename__ = 'answers'
 
-    id: Mapped[str] = mapped_column(primary_key=True, autoincrement=False)
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    uuid: Mapped[str] = mapped_column(index=True, autoincrement=False)
+
     text: Mapped[Optional[str]] = mapped_column(nullable=True, default=None)
     status: Mapped[AnswerStatus] = mapped_column(nullable=False, default=AnswerStatus.UNCHECKED)
 
@@ -166,3 +178,16 @@ class UserTestParticipation(Base):
     test_id: Mapped[str] = mapped_column(nullable=False, index=True)
     status: Mapped[UserTestParticipationStatus] = mapped_column(
         nullable=False, default=UserTestParticipationStatus.NOT_PASSED)
+
+
+class ClassroomTestConnection(Base):
+    __tablename__ = 'classroom_test_connection'
+
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+
+    test_id: Mapped[int] = mapped_column(ForeignKey("tests.id"))  # Child
+    test: Mapped[Test] = relationship(back_populates="classroom_test_connections", uselist=False)
+
+    classroom_id: Mapped[int] = mapped_column(ForeignKey("classrooms.id"))  # Child
+    classroom: Mapped[Classroom] = relationship(back_populates="classroom_test_connections", uselist=False)
+
