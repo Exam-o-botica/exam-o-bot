16e07c2d0c6d40a0566d96b11057d82560349af2
arsenyvolodko <arvolodko@gmail.com>
2024-01-28 14:07:56 +0300

    added start-command-links classroom and test functionality

--- src/examobot/bot/consts.py
+++ src/examobot/bot/consts.py
@@ -1,2 +1,5 @@
 MAIN_MENU_TEXT = 'main menu'
 START_TEXT = 'welcome to examobot!'
+
+SUCCESSFULLY_ADDED_TO_CLASSROOM = 'You have successfully joinded classroom{}'
+SUCCESSFULLY_ADDED_TO_TESTS = 'you was successfully joined to test "{}" participants. now you can pass it'
--- src/examobot/db/tables.py
+++ src/examobot/db/tables.py
@@ -1,7 +1,7 @@
 from enum import Enum
 from typing import List, Optional
 
-from sqlalchemy import ForeignKey, Column, Table, LargeBinary, BigInteger
+from sqlalchemy import ForeignKey, Column, LargeBinary, BigInteger
 from sqlalchemy.ext.asyncio import AsyncAttrs
 from sqlalchemy.orm import DeclarativeBase
 from sqlalchemy.orm import Mapped
@@ -40,28 +40,11 @@ class Base(AsyncAttrs, DeclarativeBase):
     pass
 
 
-classroom_user_table = Table(
-    "classroom_user_table",
-    Base.metadata,
-    Column("user_id", ForeignKey("users.id"), primary_key=True),
-    Column("classroom_id", ForeignKey("classrooms.id"), primary_key=True),
-)
-
-test_user_table = Table(
-    "test_user_table",
-    Base.metadata,
-    Column("user_id", ForeignKey("users.id"), primary_key=True),
-    Column("test_id", ForeignKey("tests.id"), primary_key=True),
-)
-
-
 class User(Base):
     __tablename__ = "users"
 
     id: Column = Column(BigInteger,
                         primary_key=True, autoincrement=False, nullable=False, index=True)
-
-    # id: Mapped[int] = mapped_column(primary_key=True, autoincrement=False, nullable=False, index=True)
 
     username: Mapped[str] = mapped_column(nullable=False)
     name: Mapped[str] = mapped_column(nullable=False)
@@ -69,12 +52,6 @@ class User(Base):
     await_status: Mapped[str] = mapped_column(nullable=True, default=None)
 
     answers: Mapped[List["Answer"]] = relationship(back_populates="user", cascade="all,delete")  # Parent
-
-    tests: Mapped[List['Test']] = relationship(
-        secondary=test_user_table, uselist=True, back_populates='participants')
-
-    classrooms: Mapped[List['Classroom']] = relationship(
-        secondary=classroom_user_table, uselist=True, back_populates='participants')
 
     created_classrooms: Mapped[List["Classroom"]] = relationship(
         back_populates="author",  # todo maybe we need uselist=True here, maybe not, who knows
@@ -96,9 +73,6 @@ class Classroom(Base):
     author_id: Mapped[int] = mapped_column(ForeignKey("users.id"), nullable=False)
     author: Mapped[User] = relationship(back_populates="created_classrooms")  # Child
 
-    participants: Mapped[List[User]] = relationship(
-        secondary=classroom_user_table, uselist=True, back_populates='classrooms')
-
     classroom_test_connections: Mapped[List["ClassroomTestConnection"]] = (
         relationship(back_populates="classroom", cascade="all,delete", uselist=True))  # Parent
 
@@ -114,15 +88,23 @@ class Test(Base):
     deadline: Mapped[int] = mapped_column(nullable=False, default=-1)
     attempts_number: Mapped[int] = mapped_column(nullable=False, default=1)
     status_set_by_author: Mapped[TestStatus] = mapped_column(nullable=False, default=TestStatus.AVAILABLE)
+    link: Mapped[str] = mapped_column(nullable=False)
+    meta_data: Mapped[Optional[str]] = mapped_column(nullable=False, default=None)
 
     author_id: Mapped[BigInteger] = mapped_column(ForeignKey("users.id"), nullable=False)
     author: Mapped[User] = relationship(back_populates="created_tests")  # Child
 
-    participants: Mapped[List[User]] = relationship(
-        secondary=test_user_table, uselist=True, back_populates='tests')
     tasks: Mapped[List["Task"]] = relationship(back_populates="test", cascade="all,delete")  # Parent
     classroom_test_connections: Mapped[List["ClassroomTestConnection"]] = (
         relationship(back_populates="test", cascade="all,delete", uselist=True))  # Parent
+
+
+class UserClassroomParticipation(Base):
+    __tablename__ = 'user_classroom_participation'
+
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    user_id: Column = Column(ForeignKey("users.id"), nullable=False)
+    classroom_id: Mapped[int] = mapped_column(ForeignKey("classrooms.id"), nullable=False)
 
 
 class Task(Base):
@@ -146,15 +128,6 @@ class Task(Base):
     test: Mapped[Test] = relationship(back_populates="tasks")  # Child
 
 
-# class Attempt(Base):
-#     __tablename__ = 'attempts'
-#
-#     user_id: Mapped[BigInteger] = mapped_column(ForeignKey("users.id"), nullable=False)
-#     user: Mapped[User] = relationship(back_populates="attempts")  # Child
-#
-#     test_id: Mapped[int] = mapped_column(ForeignKey("tests.id"), nullable=False)
-#     test: Mapped[Test] = relationship(back_populates="attempts")
-
 class Answer(Base):
     __tablename__ = 'answers'
 
@@ -175,8 +148,8 @@ class UserTestParticipation(Base):
     __tablename__ = 'user_test_participation'
 
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
-    user_id: Column = Column(BigInteger, nullable=False, index=True)
-    test_id: Mapped[str] = mapped_column(nullable=False, index=True)
+    user_id: Column = Column(ForeignKey("users.id"), nullable=False)
+    test_id: Mapped[int] = mapped_column(ForeignKey("tests.id"), nullable=False)
     status: Mapped[UserTestParticipationStatus] = mapped_column(
         nullable=False, default=UserTestParticipationStatus.NOT_PASSED)
 
