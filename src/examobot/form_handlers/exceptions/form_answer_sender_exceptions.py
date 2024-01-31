e2f0b2d15f89dede4272886da2454642c155e50e
Ultimatereo <mansursult9@gmail.com>
2024-01-31 14:09:36 +0300

    Added exceptions

---
+++ src/examobot/form_handlers/exceptions/form_answer_sender_exceptions.py
@@ -0,0 +1,58 @@
+from typing import Any
+
+MAX_LENGTH_STR = 50
+
+
+class FormAnswerSenderException(Exception):
+    pass
+
+
+class HTTPError(FormAnswerSenderException):
+    def __init__(self, url: str):
+        self.url = url
+
+    def __str__(self):
+        return (f"Error happened when trying to send answers to form via link:\n"
+                f"{self.url}")
+
+
+def _shorten(data: Any) -> str:
+    data_str = str(data)
+    length = len(data_str)
+    if length < MAX_LENGTH_STR:
+        return data_str
+    else:
+        return data_str[:MAX_LENGTH_STR] + "..."
+
+
+class JSONParseError(FormAnswerSenderException):
+    def __init__(self, metadata):
+        self.metadata_text = _shorten(metadata)
+
+    def __str__(self):
+        return (f"Error happened when trying to parse json:\n"
+                f"{self.metadata_text}")
+
+
+class URLError(FormAnswerSenderException):
+    def __str__(self):
+        return f"Error happened when trying to create link for sending form."
+
+
+class TestCompleteFailError(FormAnswerSenderException):
+    def __str__(self):
+        return f"Test was not completed! Probably some required fields were missing."
+
+
+class HTMLError(FormAnswerSenderException):
+    def __init__(self, html_str):
+        self.text = _shorten(html_str)
+
+    def __str__(self):
+        return (f"Error occurred when parsing html of the result after sending answer.\n"
+                f"The given html to parse: {self.text}")
+
+
+class SendFailError(FormAnswerSenderException):
+    def __str__(self):
+        return f'Sending answers was failed. Please check that you wrote everything correctly according to form.'
--- src/examobot/form_handlers/form_answer_sender.py
+++ src/examobot/form_handlers/form_answer_sender.py
@@ -2,77 +2,98 @@ import asyncio
 import json
 from typing import Any
 
+import exceptions
 import requests
-from googleapiclient import errors
 from lxml import html
-
-
-# TODO: Change logic of class. It should mostly return nothing and throw custom exceptions so that later on
-#  it could be caught and handled properly depending on the exception
 
 
 class FormAnswerSender:
     @staticmethod
     def _create_send_url(data: dict[str, Any], answers: dict[int, str]) -> str:
-        base_url = data["responderUri"]
-        form_id = base_url.split("/")[-2]
-        new_url = f'https://docs.google.com/forms/d/e/{form_id}/formResponse?&submit=Submit?&'
-        for entry_id, answer in answers.items():
-            entry_param = f'entry.{entry_id}={answer}&'
-            new_url += entry_param
-        return new_url
+        try:
+            base_url = data["responderUri"]
+            form_id = base_url.split("/")[-2]
+            new_url = f'https://docs.google.com/forms/d/e/{form_id}/formResponse?&submit=Submit?&'
+            for entry_id, answer in answers.items():
+                entry_param = f'entry.{entry_id}={answer}&'
+                new_url += entry_param
+            return new_url
+        except Exception as e:
+            raise exceptions.URLError from e
 
     @staticmethod
-    def _determine_if_test_is_complete(html_str: str) -> bool:
+    def _determine_if_test_is_complete(html_str: str):
         try:
             # Parse the HTML string
             tree = html.fromstring(html_str)
 
             # Check if the page contains a form tag
             form_tags = tree.xpath('//form')
-
-            if form_tags:
-                return False
-            else:
-                return True
         except Exception as e:
-            print(f"Error parsing HTML: {e}")
-            return False
+            raise exceptions.HTMLError(html_str) from e
+        if form_tags:
+            raise exceptions.SendFailError()
 
     @staticmethod
-    async def send_answer(metadata: str, answers: dict[int, str]) -> bool:
-        try:
-            data = json.loads(metadata)
-            url = FormAnswerSender._create_send_url(data, answers)
-            resp = requests.get(url)
-            resp.raise_for_status()
-            return FormAnswerSender._determine_if_test_is_complete(resp.text)
-        except (errors.HttpError,):
-            pass
+    async def send_answer_raw(metadata: str, answers: dict[int, str]):
+        data = FormAnswerSender._parse_json(metadata)
+        await FormAnswerSender.send_answer(data, answers)
 
     @staticmethod
-    async def send_answer_(data: dict[str, Any], answers: dict[int, str]):
+    async def send_answer(data: dict[str, Any], answers: dict[int, str]):
+        url = FormAnswerSender._create_send_url(data, answers)
+        resp = requests.get(url)
+        FormAnswerSender._raise_for_status_custom_exception(resp, url)
+        # FormAnswerSender._determine_if_test_is_complete(resp.text)
+
+    @staticmethod
+    def _raise_for_status_custom_exception(resp, url):
         try:
-            url = FormAnswerSender._create_send_url(data, answers)
-            resp = requests.get(url)
+            print(resp.status_code)
             resp.raise_for_status()
-            FormAnswerSender._determine_if_test_is_complete(resp.text)
+        except requests.HTTPError as e:
+            raise exceptions.HTTPError(url) from e
+
+    @staticmethod
+    def _parse_json(metadata):
+        try:
+            json.loads(metadata)
         except Exception as e:
-            raise
+            raise exceptions.JSONParseError(metadata) from e
 
 
-def main():
-    form_answer_sender = FormAnswerSender()
-    with open("examples/exampleForm.json", 'r') as file:
-        data = json.load(file)
-    answers = {
+def correct_send(form_answer_sender, data):
+    correct_answers = {
         1868536814: "1965",
         1059122755: "%D0%91%D0%B5%D0%B1%D1%80%D0%B0",
         748499805: "%D0%91%D0%B8%D0%B1%D0%B0",
         857106950: "%D0%92%D0%B0%D1%80%D0%B8%D0%B0%D0%BD%D1%82+1",
         422932123: "%D0%92%D0%B0%D1%80%D0%B8%D0%B0%D0%BD%D1%82+1"
     }
-    print(form_answer_sender.send_answer_(data, answers))
+    asyncio.run(form_answer_sender.send_answer(data, correct_answers))
+
+
+def incorrect_send_fail(form_answer_sender, data):
+    incorrect_answers = {
+        1868536814: "1965",
+        10522755: "%D0%91%D0%B5%D0%B1%D1%80%D0%B0",
+        748499805: "%D0%91%D0%B8%D0%B1%D0%B0",
+        857106950: "%D0%92%D0%B0%D1%80%D0%B8%D0%B0%D0%BD%D1%82+1",
+        422932123: "%D0%92%D0%B0%D1%80%D0%B8%D0%B0%D0%BD%D1%82+1"
+    }
+    try:
+        asyncio.run(form_answer_sender.send_answer(data, incorrect_answers))
+    except (exceptions.SendFailError, exceptions.HTTPError) as e:
+        print(e)
+        print("If this error was caught then we successfully sent answers but they were in a wrong format.")
+
+
+def main():
+    form_answer_sender = FormAnswerSender()
+    with open("examples/exampleForm.json", 'r') as file:
+        data = json.load(file)
+    correct_send(form_answer_sender, data)
+    incorrect_send_fail(form_answer_sender, data)
 
 
 if __name__ == "__main__":
